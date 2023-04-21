27afbfee2276d079d8780fe00dd11426d01bb425
caffranksereia <fabio@yaayoo.com.br>
2023-04-21 10:18:20 -0300

    criado Login como page

---
+++ src/Login.tsx
@@ -0,0 +1,21 @@
+export  default function Login(){
+    return(
+        <div className="body">
+            <div className="LoginAndSenha">
+                <form action="">
+                    <label htmlFor="">
+                        Login
+                    </label>
+                    <input type="email" name="" id="" />
+
+                    <label htmlFor="">
+                        Passwords
+                    </label>
+                    <input type="password" name="" id="" />
+                </form>
+            </div>
+
+           <button>Login</button>
+        </div>
+    ) 
+}
