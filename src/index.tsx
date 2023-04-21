d19785f956b82f8d4e80d99b147db94522b538f5
caffranksereia <fabio@yaayoo.com.br>
2023-04-21 10:04:55 -0300

    delete

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import HomePage from './HomePage';
+import Page404 from './Page404';
+import   {
+  createBrowserRouter,
+  RouterProvider,
+  Switch
+} from 'react-router-dom';
+
+import Cadastro from './Cadastro';
+import Login from './Login';
+import DetailUser from './DetailUser'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <HomePage/>,
+    errorElement:<Page404/>,
+    children:[
+      {
+        path:"/Login",
+        element: <Login/>,
+      },
+      {
+        path:"/Cadastro",
+        element: <Cadastro/>,
+      },
+      {
+        path:"/Profile",
+        element: <DetailUser/>,
+      },
+      {
+        path:"/Todo/",
+        element: <DetailUser/>,
+      },
+      {
+        path:"/Todo/:id",
+        element: <DetailUser/>,
+      },
+    ]
+  },
+  
+
+
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
